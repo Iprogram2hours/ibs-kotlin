b135d4d0220ddfffe851fd28f44383fbb28c87f4
Iprogram2hours <tmex2u3e>
2021-07-11 15:32:14 +0300

    resolve task #23

---
+++ src/ru/ibs/kotlin/module4/Vehicle.kt
@@ -0,0 +1,4 @@
+package ru.ibs.kotlin.module4
+
+data class Vehicle(val brand:String, val color:String, val number:String) {
+}
--- src/ru/ibs/kotlin/module4/module4.kt
+++ src/ru/ibs/kotlin/module4/module4.kt
@@ -2,4 +2,7 @@ package ru.ibs.kotlin.module4
 
 fun module4() {
     println("Module 4")
+
+    val vehicle = Vehicle("Toyota", "White", "A111AA78")
+    println(vehicle.toString())
 }
