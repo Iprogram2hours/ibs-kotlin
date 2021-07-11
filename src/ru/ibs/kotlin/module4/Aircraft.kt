44809ef6e394b5617d03fc0a7fc78a74e135ce84
Iprogram2hours <tmex2u3e>
2021-07-11 15:19:05 +0300

    resolve task #22

--- src/ru/ibs/kotlin/module4/Aircraft.kt
+++ src/ru/ibs/kotlin/module4/Aircraft.kt
@@ -2,9 +2,13 @@ package ru.ibs.kotlin.module4
 
 abstract class Aircraft(_number:String, _tankCapacity:Long, _maxFlightRange:Double) {
 
-    var number:String = _number
-    var tankCapacity:Long = _tankCapacity
-    var maxFlightRange:Double = _maxFlightRange
-    val fuelConsumption:Double
+    protected var number:String = _number
+    protected var tankCapacity:Long = _tankCapacity
+    protected var maxFlightRange:Double = _maxFlightRange
+    protected val fuelConsumption:Double
         get() = maxFlightRange/tankCapacity
+
+    open fun info() {
+        println("Number: $number; tankCapacity: $tankCapacity; maxFlightRange: $maxFlightRange; fuelConsumption: $fuelConsumption")
+    }
 }
--- src/ru/ibs/kotlin/module4/Boeing747.kt
+++ src/ru/ibs/kotlin/module4/Boeing747.kt
@@ -3,4 +3,8 @@ package ru.ibs.kotlin.module4
 class Boeing747(_number:String, _tankCapacity:Long, _maxFlightRange:Double, override var passengerCapacity: Int)
     : Aircraft(_number, _tankCapacity, _maxFlightRange), Passenger {
 
+    override fun info() {
+        println("Number: $number; tankCapacity: $tankCapacity; maxFlightRange: " +
+                "$maxFlightRange; fuelConsumption: $fuelConsumption; passengerCapacity: $passengerCapacity")
+    }
 }
