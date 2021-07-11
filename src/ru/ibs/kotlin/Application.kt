b499decf749d48e551438b1aa1cd1fbda7fcf754
Iprogram2hours <tmex2u3e>
2021-07-11 14:10:48 +0300

    init module 4

--- src/ru/ibs/kotlin/Application.kt
+++ src/ru/ibs/kotlin/Application.kt
@@ -3,10 +3,12 @@ package ru.ibs.kotlin
 import ru.ibs.kotlin.module1.module1
 import ru.ibs.kotlin.module2.module2
 import ru.ibs.kotlin.module3.module3
+import ru.ibs.kotlin.module4.module4
 
 
 fun main(args: Array<String>) {
     module1()
     module2()
     module3()
+    module4()
 }
---
+++ src/ru/ibs/kotlin/module4/module4.kt
@@ -0,0 +1,5 @@
+package ru.ibs.kotlin.module4
+
+fun module4() {
+    println("Module 4")
+}
